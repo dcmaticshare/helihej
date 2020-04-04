ed9da6010a043ca78f1b1c674d24790275bd2471
dcmaticshare <47565432+dcmaticshare@users.noreply.github.com>
2020-04-04 11:57:23 +0200

    Add files via upload

---
+++ js/translator.js
@@ -0,0 +1,61 @@
+var dictionary = {
+  'herstelling': {
+    'nl': 'Herstelling',
+    'fr': 'Réparer',
+  },
+  'onderhoud': {
+    'nl': 'Onderhoud',
+    'fr': 'Entretien',
+  },
+  'contact': {
+    'nl': 'Contact',
+    'fr': 'Contacter',
+  },
+  'herstellingaanvragen': {
+    'nl': 'herstelling aanvragen',
+    'fr': 'demander réparation',
+  },
+  'onsbedrijf': {
+    'nl': 'Ons bedrijf',
+    'fr': 'Notre entreprise',
+  },
+  'voorschriften': {
+    'nl': 'Voorschriften',
+    'fr': 'Réglementations',
+  },
+  'producten': {
+    'nl': 'Producten',
+    'fr': 'Produits',
+  },
+  'referenties': {
+    'nl': 'Referenties',
+    'fr': 'Références',
+  },
+  'vacatures': {
+    'nl': 'Vacatures',
+    'fr': 'Postes vacants',
+  }
+  
+};
+var langs = ['nl', 'fr'];
+var current_lang_index = 0;
+var current_lang = langs[current_lang_index];
+
+window.change_lang = function (langIndexSelected) {
+  
+  current_lang_index = langIndexSelected;
+  current_lang = langs[current_lang_index];
+  $(".dropdown-toggle").each(function () {
+    $(this).text(current_lang);
+  });
+  translate();
+}
+
+function translate() {
+  $("[data-translate]").each(function () {
+    var key = $(this).data('translate');
+    $(this).html(dictionary[key][current_lang] || "N/A");
+  });
+}
+
+translate();
