e5696fb41b69358c6eb6c47a8b1f89f486be1bbc
dcmaticshare <47565432+dcmaticshare@users.noreply.github.com>
2020-04-29 21:38:31 +0200

    Add files via upload

--- js/translator.js
+++ js/translator.js
@@ -1,8 +1,22 @@
 var dictionary = {
+  'aantal': {
+    'nl': 'Aantal',
+    'fr': 'Nombre',
+  },
+  'aantalverdiepen': {
+    'nl': 'Aantal verdiepingen',
+    'fr': 'Nombre d\'étages',
+  },
+  
   'aanvraagherstelling': {
     'nl': 'Aanvraag herstelling',
     'fr': 'Demander une réparation',
   },
+  'aanvraagonderhoud': {
+    'nl': 'Aanvraag onderhoud',
+    'fr': 'Demander une maintenance',
+  },
+  
   'adres*': {
     'nl': 'Adres *',
     'fr': 'Adresse *',
@@ -30,7 +44,7 @@ var dictionary = {
   },
   'bannerRepair': {
     'nl': 'Probleem? Helioscreen Service staat klaar!',
-    'fr': 'Demander réparation',
+    'fr': 'Un problème? Le service Helioscreen est prêt!',
   },
 
   'bedrijf': {
@@ -40,6 +54,10 @@ var dictionary = {
   'bereikbaarheidzonwering': {
     'nl': 'Bereikbaarheid',
     'fr': 'Accessibilité',
+  },
+  'btwnr': {
+    'nl': 'BTW nummer',
+    'fr': 'Numéro de TVA',
   },
   'btwnr*': {
     'nl': 'BTW nummer *',
@@ -57,6 +75,15 @@ var dictionary = {
     'nl': 'Contactgegevens',
     'fr': 'Coordonnées',
   },
+  'emailadres': {
+    'nl': 'E-mailadres',
+    'fr': 'Adresse e-mail',
+  },
+  'detailszon': {
+    'nl': 'DETAILS ZONWERING',
+    'fr': 'DÉTAILS D\'AUVENT',
+  },
+  
   'facturatiegegevens': {
     'nl': 'Facturatiegegevens',
     'fr': 'Informations de facturation',
@@ -74,6 +101,11 @@ var dictionary = {
     'nl': 'Gelijkvloers / Terras (evt met kleine trapladder)',
     'fr': 'Rez-de-chaussée / terrasse (éventuellement avec petit escabeau)',
   },
+  'gemeenteenpost': {
+    'nl': 'Gemeente en postcode',
+    'fr': 'Municipalité et code postal',
+  },
+  
   'gevelinstallatie': {
     'nl': 'Gevelinstallatie',
     'fr': 'Installation de façade',
@@ -90,6 +122,18 @@ var dictionary = {
   'herstelling': {
     'nl': 'Herstelling',
     'fr': 'Réparation',
+  },
+  'herstellingen': {
+    'nl': 'Herstellingen',
+    'fr': 'Réparations',
+  },
+  'herstellingText1': {
+    'nl': 'Wanneer u een defect of storing aan een zonnescherm of motor opmerkt is het van belang dat u de zonwering zo snel mogelijk op laat zodat het niet verder kan beschadigen. Neem vervolgens contact op met ons team Service en Onderhoud. Op basis van de situatie plant onze medewerker Service en Onderhoud een interventie in.',
+    'fr': 'Si vous constatez un défaut ou un dysfonctionnement d\'un pare-soleil ou d\'un moteur, il est important de laisser le pare-soleil allumé dès que possible afin qu\'il ne puisse plus être endommagé. Contactez ensuite notre équipe de service et de maintenance. Notre employé du service et de la maintenance planifie une intervention en fonction de la situation.',
+  },
+  'herstellingText2': {
+    'nl': 'Een team van specialisten staat voor u klaar om uw zonwering opnieuw in goede staat te brengen. Onze ervaren technici overlopen graag met u de verschillende mogelijkheden en beste aanpak, terwijl u voor al uw vragen terecht kan bij onze medewerkers.',
+    'fr': 'Une équipe de spécialistes est prête à remettre votre protection solaire en bon état. Nos techniciens expérimentés sont heureux de discuter des différentes options et de la meilleure approche avec vous, tandis que vous pouvez contacter nos employés pour toutes vos questions.',
   },
   'herstellingaanvragen': {
     'nl': 'Herstelling aanvragen',
@@ -117,6 +161,18 @@ var dictionary = {
     'fr': 'Je voudrai un contrat de maintenance',
   },
 
+  'indexCardBody1': {
+    'nl': 'Zonwering hangt het hele jaar door in alle weersomstandigheden buiten. Hierdoor kunnen er defecten ontstaan aan bijvoorbeeld het doek of de motor. Omdat zonwering gewoon moet werken komen wij bij een defect zo snel mogelijk in actie.',
+    'fr': 'La protection solaire est suspendue toute l\'année dans toutes les conditions météorologiques. Cela peut entraîner des défauts, par exemple, dans le tissu ou le moteur. Parce que les stores doivent simplement fonctionner, nous intervenons dès que possible en cas de défaut.',
+  },
+  'indexCardBody2': {
+    'nl': 'Niets is zo vervelend als zonwering die niet werkt op het moment dat het nodig is. Preventief onderhoud kan onverwachte ongemakken voorkomen en verlengt ontegensprekelijk de levensduur van uw zonwering.',
+    'fr': 'Rien n\'est plus ennuyeux que les auvents qui ne fonctionnent pas lorsque vous en avez besoin. Un entretien préventif peut éviter des désagréments inattendus et prolonge indéniablement la durée de vie de votre protection solaire.',
+  },
+  'indexCardBody3': {
+    'nl': 'Als marktleider in service en onderhoud heeft Helioscreen ervaring in alle branches en sectoren. Neem zeker een kijkje!',
+    'fr': 'En tant que leader du marché du service et de la maintenance, Helioscreen possède une expérience dans toutes les branches et secteurs. N\'oubliez pas de jeter un œil!',
+  },
   'inregie': {
     'nl': 'In regie (verkennende interventie - indien nodig - is steeds betalend)',
     'fr': 'En contrôle (intervention exploratoire - si nécessaire - est toujours facturée)',
@@ -136,6 +192,10 @@ var dictionary = {
     'nl': 'Ja',
     'fr': 'Oui',
   },
+  'kandidatuur': {
+    'nl': 'Stel hier je kandidatuur',
+    'fr': 'Soumettez votre candidature ici',
+  },
   'kennisvoorwaarden': {
     
     'nl': 'Ja, ik heb kennis genomen van de <a class="underline" data-toggle="modal" data-target="#exampleModal2" href=\"#\"\>algemene voorwaarden</a>&nbsp;*',
@@ -154,13 +214,46 @@ var dictionary = {
     'nl': 'Klantnummer',
     'fr': 'Numéro de client',
   },
+  'knikarmschermen': {
+    'nl': 'Knikarmschermen',
+    'fr': 'Auvents à bras articulé',
+  },
+  'leeftijdzon': {
+    'nl': 'Leeftijd zonwering',
+    'fr': 'Âge de l\'auvent',
+  },
+  
+  'leesmeer': {
+    'nl': 'Lees meer',
+    'fr': 'Lire plus',
+  },
+  'markiezen': {
+    'nl': 'Markiezen',
+    'fr': 'Auvents',
+  },
   'medecontractant': {
     'nl': 'Medecontractant',
     'fr': 'Co-contractant',
   },
+  'meerdere': {
+    'nl': 'Meerdere',
+    'fr': 'Plusieurs',
+  },
+  'meldhierstoring': {
+    'nl': 'Meld hier uw storing.<br>Wij komen zo snel mogelijk in actie om het op te lossen.',
+    'fr': 'Signalez votre dysfonctionnement ici. <br> Nous prendrons des mesures dès que possible pour le résoudre.',
+  },
   'merkzonwering': {
     'nl': 'Merk van de zonwering *',
     'fr': 'Marque de l\'auvent *',
+  },
+  'motivatie': {
+    'nl': 'Dit is mijn motivatie',
+    'fr': 'Ci-dessous ma motivation',
+  },
+  'naam': {
+    'nl': 'Naam',
+    'fr': 'Nom',
   },
   'naam*': {
     'nl': 'Naam *',
@@ -169,6 +262,10 @@ var dictionary = {
   'naamcontactterplaatse*': {
     'nl': 'Naam contactpersoon ter plaatse *',
     'fr': 'Nom de la personne de contact sur place *',
+  },
+  'naarherstelformulier': {
+    'nl': 'Naar het herstelformulier',
+    'fr': 'Vers le formulaire réparation',
   },
 
   'nee': {
@@ -180,6 +277,11 @@ var dictionary = {
     'fr': 'Devis (l\'intervention exploratoire pour l\'établissement du devis est toujours facturée)',
   },
 
+  'offerteAanvraagtekst': {
+    'nl': 'Wilt u een vrijblijvende offerte ontvangen?<br>Vul dan het formulier hieronder in en wij zorgen ervoor!',
+    'fr': 'Souhaitez-vous recevoir un devis gratuit?<br>Remplissez le formulaire ci-dessous et nous nous en occuperons!',
+  },
+
   'omschrijvingdefect': {
     'nl': 'Omschrijving defect',
     'fr': 'Description du défaut',
@@ -189,12 +291,84 @@ var dictionary = {
     'nl': 'Onderhoud',
     'fr': 'Entretien',
   },
-
-
+  'onderhoudTitle': {
+    'nl': 'Voorkom problemen met een onderhouds-overeenkomst',
+    'fr': 'Évitez les problèmes avec un contrat de maintenance',
+  },
+  
+  'onderhoudAanvragenButton': {
+    'nl': 'Onderhoudsvoorstel aanvragen',
+    'fr': 'Demander une proposition',
+  },
+  'onderhoudcard1Title': {
+    'nl': 'Preventieve onderhouds-overeenkomst',
+    'fr': 'Contrat de maintenance préventive',
+  },
+  'onderhoudcard2Title': {
+    'nl': 'Inspectie-overeenkomst',
+    'fr': 'Accord d\'inspection',
+  },
+  'onderhoudcard3Title': {
+    'nl': 'Raamovereenkomst',
+    'fr': 'Accord-cadre',
+  },
+  'onderhoudcard4Title': {
+    'nl': 'Overeenkomst op maat',
+    'fr': 'Accord personnalisé',
+  },
+  'onderhoudcard1Body': {
+    'nl': 'Jaarlijks wordt de zonwering grondig geïnspecteerd en onderhouden tegen een forfaitair bedrag. Hieronder vallen alle onderhoudswerken, -producten en kleine onderdelen. De ‘grotere’ herstellingen (motor, doek, profielen, stuurkasten, …) vallen buiten dit contract.',
+    'fr': 'Les stores sont soigneusement inspectés et entretenus chaque année pour un montant fixe. Cela comprend tous les travaux de maintenance, les produits et les petites pièces. Les "grosses" réparations (moteur, toile, profilés, boîtiers de commande, ...) ne sont pas couvertes par ce contrat.',
+  },
+  'onderhoudcard2Body': {
+    'nl': 'Elk jaar wordt er een grondige controle uitgevoerd, die ons toelaat de nodige onderhouds- of herstellingswerken te definiëren, omschrijven en begroten.',
+    'fr': 'Chaque année, un contrôle approfondi est effectué, qui nous permet de définir, décrire et budgétiser les travaux de maintenance ou de réparation nécessaires.',
+  },
+  'onderhoudcard3Body': {
+    'nl': 'Een raamovereenkomst of raamcontract biedt de mogelijk om belangrijke zaken zoals tarieven, interventietermijnen, kortingen, … in onderling overleg vast te leggen. Dit kan gekoppeld worden aan preventief onderhoud of uitsluitend op regie basis. In beide gevallen geniet u van de standaard voordelen die bij een onderhoudsovereenkoms gelden.',
+    'fr': 'Un accord-cadre ou un contrat-cadre permet de déterminer des questions importantes telles que les tarifs, les périodes d\'intervention, les remises, etc. en concertation. Cela peut être lié à la maintenance préventive ou uniquement sur une base de gestion. Dans les deux cas, vous bénéficiez des avantages standard qui s\'appliquent avec un contrat de maintenance.',
+  },
+  'onderhoudcard4Body': {
+    'nl': 'Onze Sales Engineers staan u graag bij om in overleg een overeenkomst uit werken die volledig aansluit bij uw wensen en noden.',
+    'fr': 'Nos ingénieurs commerciaux sont heureux de vous aider à trouver un accord qui correspond pleinement à vos souhaits et besoins.',
+  },
+  'onderhoudTekst1a': {
+    'nl': 'Onderhoudscontract bij Helioscreen levert tal van voordelen:',
+    'fr': 'Le contrat de maintenance avec Helioscreen offre de nombreux avantages:',
+  },
+  'onderhoudTekst1b': {
+    'nl': 'Prioriteit bij herstellingen',
+    'fr': 'Priorité de réparation',
+  },
+  'onderhoudTekst1c': {
+    'nl': 'Korting op wisselstukken',
+    'fr': 'Remise sur les pièces détachées',
+  },
+  'onderhoudTekst1d': {
+    'nl': 'Verlengde garantie bij nieuwe installaties',
+    'fr': 'Garantie prolongée pour les nouvelles installations',
+  },
+  'onderhoudTekst1e': {
+    'nl': 'Onderhoud en herstellingen op alle merken',
+    'fr': 'Maintenance et réparations sur toutes les marques',
+  },
+  'onderhoudTekst2': {
+    'nl': 'Uw tijd is kostbaar en drukbezet. Laat u daarom ontzorgen over uw zonwering door de specialisten van Helioscreen. Met een onderhoudsovereenkomst wordt alles voor u geregeld, steeds in overleg, maar zonder dat u hiervoor iets hoeft te ondernemen. Onze medewerkers laten u weten wanneer het tijd is om de installatie te onderhouden, en u wordt vanzelf op de hoogte gebracht van de staat van uw installatie met een gedetailleerd onderhoudsrapport. Uiteraard geniet u ook tal van bovenstaande voordelen.',
+    'fr': 'Votre temps est précieux et occupé. Laissez-vous donc décharger de vos stores par les spécialistes d\'Helioscreen. Avec un contrat de maintenance, tout est arrangé pour vous, toujours en concertation, mais sans que vous ayez à faire quoi que ce soit. Notre personnel vous informera du moment de l\'entretien de l\'installation et vous serez automatiquement informé de l\'état de votre installation avec un rapport d\'entretien détaillé. Bien sûr, vous bénéficiez également de nombreux avantages ci-dessus.',
+  },
+  'onderhoudTekst3': {
+    'nl': 'Verschillende factoren (budget, bereikbaarheid, veiligheid, ...) spelen een grote rol bij het maken van de juiste keuze, en zijn bovendien voor elke klant verschillend. U heeft de keuze uit verschillende formules, en er kan ook steeds een voorstel op maat uitgewerkt worden.',
+    'fr': 'Différents facteurs (budget, accessibilité, sécurité, ...) jouent un rôle majeur pour faire le bon choix, et sont également différents pour chaque client. Vous pouvez choisir parmi différentes formules et vous pouvez toujours préparer une proposition sur mesure.',
+  },
   'onsbedrijf': {
     'nl': 'Ons bedrijf',
     'fr': 'Notre entreprise',
   },
+  'parasols': {
+    'nl': 'Parasols',
+    'fr': 'Parapluies',
+  },
+  
   'producten': {
     'nl': 'Producten',
     'fr': 'Produits',
@@ -203,6 +377,36 @@ var dictionary = {
     'nl': 'Referenties',
     'fr': 'Références',
   },
+  'referentiesBanner1': {
+    'nl': 'Helioscreen en (na)zorg? Een perfecte match!',
+    'fr': 'Helioscreen et (après) soins? Un match parfait!',
+  },
+  'referentiesBanner2': {
+    'nl': 'De betrouwbare leverancier binnen Facility',
+    'fr': 'Le fournisseur fiable au sein de Facility',
+  },
+  'referentiesBanner3': {
+    'nl': 'Dé partner van A tot Z voor de publieke sector',
+    'fr': 'Le partenaire de A à Z pour le secteur public',
+  },
+
+  'screendoeken': {
+    'nl': 'Screendoeken',
+    'fr': 'Chiffons d\'écran',
+  },
+  
+  'serviceenonderhoud': {
+    'nl': 'Service en onderhoud',
+    'fr': 'Service et entretien',
+  },
+  'straatenhuis': {
+    'nl': 'Straat en huisnummer',
+    'fr': 'Rue et numéro de maison',
+  },
+  'telefoonnummer': {
+    'nl': 'Telefoonnummer',
+    'fr': 'Numéro de téléphone',
+  },
   'tel*': {
     'nl': 'Telefoon/GSM *',
     'fr': 'Téléphone/GSM *',
@@ -210,6 +414,10 @@ var dictionary = {
   'telcontactterplaatse*': {
     'nl': 'Telefoon contactpersoon ter plaatse *',
     'fr': 'Téléphone de la personne de contact sur place *',
+  },
+  'toelichtingaanvraag': {
+    'nl': 'Toelichting van het gevraagde onderhoud',
+    'fr': 'Explication de la maintenance demandée',
   },
   'toelichtinginterventie': {
     'nl': 'Toelichting over de gevraagde interventie',
@@ -235,12 +443,41 @@ var dictionary = {
     'nl': 'Verplaatsing Limburg, West-Vlaanderen:<br>€180,00<br>',
     'fr': 'Déplacement Limbourg, Flandre Occidentale, Namur, Hainaut, Liège:<br>€180,00<br><br>Déplacement Province du Luxembourg,Grand-Duché du Luxembourg:<br>€285,00',
   },
-
+  'typezon': {
+    'nl': 'Type zonwering',
+    'fr': 'Type d\'auvent',
+  },
+  'uitvalschermen': {
+    'nl': 'Uitvalschermen',
+    'fr': 'Écrans de baisse',
+  },
+  
+  'vacaturebanner': {
+    'nl': 'Meer 50 jaar ervaring om te delen.',
+    'fr': 'Plus de 50 ans d\'expérience à partager.',
+  },
+  'vacatureTitle': {
+    'nl': 'SERVICE TECHNIEKER BUITENDIENST',
+    'fr': 'TECHNICIEN DE SERVICE HORS SERVICE',
+  },
   
   'vacatures': {
     'nl': 'Vacatures',
     'fr': 'Postes vacants',
   },
+  'vacature1part1': {
+    'nl': '(m/v) Dagwerk – voltijds – Nederlandstalig <br> <br>Functie<br>Onderhoud en herstellingen uitvoeren aan onze bestaande binnen- en buitenzonwering-installaties, bij onze klanten in België. Je werkt meestal in team met andere collega-service techniekers.<br><br>Profiel<br>',
+    'fr': '(h/f) Travail de jour – à plein temps – Néerlandais <br> <br>Fonction<br> Effectuer la maintenance et les réparations de nos installations de protection solaire intérieure et extérieure existantes, avec nos clients en Belgique. Vous travaillez généralement en équipe avec d\'autres collègues techniciens de service. <br> <br> Profil <br>',
+  },
+  'vacature1part2': {
+    'nl': '<ul><li>Je hebt ervaring in soortgelijke functie en je bent graag op de baan.</li><li>Vroeg uit de veren, buitenwerk en werken op hoogtes schrikt je niet af.</li><li>Je bezit een rijbewijs B.</li><li>Gebruikskennis Frans is een plus.</li><li>Diploma TSO (bouw, mechanica, elektromechanica, schrijnwerk ramen).</li><li>Je bent zorgvuldig, handig en bereid dit vak (verder) aan te leren onder leiding van ervaren collega’s.</li><li>Stipt, betrouwbaar en collegiaal ingesteld.</li></ul>',
+    'fr': '<ul> <li> Vous avez de l\'expérience dans un poste similaire et vous aimez être au travail. </li> <li> Se lever tôt, travailler à l\'extérieur et travailler en hauteur ne vous dissuade pas. </li> <li> Vous est titulaire d\'un permis de conduire B. </li> <li> La connaissance du français est un plus. </li> <li> Diplôme TSO (construction, mécanique, électromécanique, menuiserie fenêtres). </li> <li> Vous êtes prudent, pratique et désireux d\'apprendre (plus loin) ce cours sous la direction de collègues expérimentés. </li> <li> Ponctuel, fiable et collégial. </li> </ul>',
+  },
+  'vacature1part3': {
+    'nl': '<br>Ons aanbod<br>Na grondige en on-the-job training kunnen loyale, betrouwbare en dynamische doorzetters zich volop ontplooien in hun functie en samenwerken met leuke collega’s binnen een groeiend en financieel gezond bedrijf. Bovendien verdien je een aantrekkelijke verloning in functie van je ervaring en capaciteiten, aangevuld met extralegale voordelen.',
+    'fr': '<br> Notre offre <br> Après une formation approfondie et en cours d\'emploi, des getters fidèles, fiables et dynamiques peuvent développer pleinement leur position et travailler avec de bons collègues au sein d\'une entreprise en pleine croissance et financièrement saine. De plus, vous gagnez un salaire attractif en fonction de votre expérience et de vos capacités, complété par des avantages sociaux.',
+  },
+  
   'veldError': {
     'nl': 'Veld niet correct ingevuld',
     'fr': 'Texte mal rempli',
@@ -257,6 +494,11 @@ var dictionary = {
     'nl': 'Voeg foto\s/documenten toe',
     'fr': 'Ajouter des photos / documents',
   },
+  'voegfotostoenotrequired': {
+    'nl': 'Voeg foto\s/documenten toe',
+    'fr': 'Ajouter des photos / documents',
+  },
+  
   'volgensonderhoud': {
     'nl': 'Volgens onderhoudscontract - Ref.:',
     'fr': 'Selon contrat de maintenance - Réf.:',
@@ -265,6 +507,18 @@ var dictionary = {
   'voorschriften': {
     'nl': 'Voorschriften',
     'fr': 'Réglementations',
+  },
+  'voorschriftenBanner': {
+    'nl': 'Onderhouds- en reinigingsvoorschriften',
+    'fr': 'Instructions d\'entretien et de nettoyage',
+  },
+  'voorschriftenBrochure': {
+    'nl': 'Download hier de voorschriften brochure',
+    'fr': 'Téléchargez la brochure des règlements ici',
+  },
+  'voorschriftenTekst': {
+    'nl': 'Al onze producten worden geproduceerd met de meest kwalitatieve materialen en met zorg geinstalleerd door onze installateurs. Om de installatie in optimale staat te houden en zorgeloos gebruik te kunnen maken, is periodiek onderhoud noodzakelijk. In onderstaande document kan u algemene info terugvinden van al onze producten, alsook de gebruiks-, reinigings- en onderhoudsvoorschriften.',
+    'fr': 'Tous nos produits sont fabriqués avec des matériaux de la plus haute qualité et soigneusement installés par nos installateurs. Un entretien périodique est nécessaire pour maintenir l\'installation dans des conditions optimales et garantir une utilisation sans problème. Dans le document ci-dessous, vous trouverez des informations générales sur tous nos produits, ainsi que les instructions d\'utilisation, de nettoyage et d\'entretien.',
   },
   'voorwaarden': {
     'nl': 'Voorwaarden',
